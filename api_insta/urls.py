954ec0e62fa78d194f7ce6505ddee7e09c3b1ca6
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-10-04 21:24:41 +0900

    done create urls

--- api_insta/urls.py
+++ api_insta/urls.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # apiAppsのルートを指定
+    path('api/', include('api.urls')),
+    # authen/jwt/create/でJWTのトークンを発行する
+    path('authen/', include('djoser.urls.jwt')),
 ]
+
+# settings.pyに追加したMEDIA_URLとMEDIA_ROOTから画像へのルートを指定
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
